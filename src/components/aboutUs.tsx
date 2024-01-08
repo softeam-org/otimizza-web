93985ab42b606b4456acbfcfd421f27eda10f558
Uneto47 <netovasconcelos02@gmail.com>
2024-01-08 16:15:28 -0300

    feat: add about us new cards

--- src/components/aboutUs.tsx
+++ src/components/aboutUs.tsx
@@ -1,23 +1,26 @@
+import Card from "./card";
+
 import logoOtimizza from "@/assets/logoOtimizza2.svg"
+import { Buildings, Star, Eye } from "@phosphor-icons/react";
 
 function AboutUs() {
   return (
-    <div className="flex justify-between bg-[#273257] p-10 text-white items-center">
-      <div className="flex flex-col items-center space-y-12 w-1/2">
-        <div className="flex flex-col items-center">
-        <h1>SOBRE NÓS</h1>
-        {/* <p>"Seu sonho, nossa responsabilidade"</p> */}
+    <div className="flex justify-arround bg-[#273257] p-10 text-white items-center font-montserrat text-2xl space-x-30">
+      <div className="flex flex-col items-center space-y-20 flex-1">
+        <div className="flex flex-col items-center text-center">
+          <h1 className="text-4xl font-azonix">SOBRE NÓS</h1>
+          <p>Seu sonho, nossa responsabilidade</p>
         </div>
-        <div>
-        <p>Mais do que projetar, a Otimizza surge para fazer acontecer.</p>
-        <p>Usamos as mais modernas soluções de engenharia ao seu favor, afim de garantir qualidade economia e segurança na construção do seu sonho.</p>
+        <div className="space-y-2">
+          <p>Mais do que projetar, a Otimizza surge para fazer acontecer.</p>
+          <p>Usamos as mais modernas soluções de engenharia ao seu favor, afim de garantir qualidade economia e segurança na construção do seu sonho.</p>
         </div>
-        <img src={logoOtimizza} alt="Logo contendo o nome da empresa Otimizza Engenharia" className="max-w-[15rem]"/>
+        <img src={logoOtimizza} alt="Logo contendo o nome da empresa Otimizza Engenharia" className="max-w-[15rem]" />
       </div>
-      <div className="flex flex-col w-1/2">
-        <div> card1 </div>
-        <div> card2 </div>
-        <div> card3 </div>
+      <div className="flex flex-col space-y-14 flex-1 items-center">
+        <Card title={"Nosso Negocio"} text={"Projetos de Engenharia e Construção de Obras Civis."} icon={<Buildings size={24} weight="fill"/>}/>
+        <Card title={"Nossa Visao"} text={"Ser um exemplo de eficiência agilidade e qualidade nos serviços prestados."} icon={<Eye size={24} weight="fill"/>} />
+        <Card title={"Nosso Essencia"} text={"Garantir a satisfação e geração de valor sobre o que foi contratado."} icon={<Star size={24} weight="fill"/>}/>
       </div>
     </div>
   );
