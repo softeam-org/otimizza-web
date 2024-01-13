aa9897021468b7ec56b9e719a01013b6cdc5d711
Uneto47 <netovasconcelos02@gmail.com>
2024-01-13 15:17:23 -0300

    chore: responsiveness of the component card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,7 +6,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, text, icon }: CardProps) => {
   return (
-    <div className="flex flex-col w-full max-w-[20rem] font-montserrat bg-[#F3F3F3] text-[#273257] p-3 rounded-xl shadow-lg shadow-black text-lg">
+    <div className="flex flex-col w-full md:max-w-[20rem] font-montserrat bg-[#F3F3F3] text-[#273257] p-3 rounded-xl shadow-lg shadow-black text-lg">
       <div className="text-2xl font-azonix flex items-center gap-2">{icon} {title} </div>
       <div> {text} </div>
     </div>
