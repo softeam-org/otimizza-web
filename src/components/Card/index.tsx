f5fb8bff267cdc04499ac39b3e6be75569f4d8f1
Uneto47 <netovasconcelos02@gmail.com>
2024-01-16 16:10:08 -0300

    refactor: improve code structure and add responsive design

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,7 +6,7 @@ interface CardProps {
 
 function Card({ title, text, icon }: CardProps): JSX.Element{
   return (
-    <div className="flex flex-col w-full md:max-w-[20rem] font-montserrat bg-[#F3F3F3] text-[#273257] p-3 rounded-xl shadow-card text-lg xsm:text-xl">
+    <div className="flex flex-col w-full md:max-w-[20rem] font-montserrat bg-snow-white text-primary p-3 rounded-xl shadow-card text-lg xsm:text-xl">
       <header className="text-lg xsm:text-xl font-azonix flex items-center gap-2">
         {icon} {title}
       </header>
