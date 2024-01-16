69ce1b338b94fe8d7282a88ae46aaad2fc8b9920
Uneto47 <netovasconcelos02@gmail.com>
2024-01-16 15:56:50 -0300

    refactor: improve code structure and add responsive design

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -4,13 +4,16 @@ interface CardProps {
   icon: React.ReactNode;
 }
 
-const Card: React.FC<CardProps> = ({ title, text, icon }: CardProps) => {
+function Card({ title, text, icon }: CardProps): JSX.Element{
   return (
-    <div className="flex flex-col w-full md:max-w-[20rem] font-montserrat bg-[#F3F3F3] text-[#273257] p-3 rounded-xl shadow-lg shadow-black text-lg">
-      <div className="text-2xl font-azonix flex items-center gap-2">{icon} {title} </div>
-      <div> {text} </div>
+    <div className="flex flex-col w-full md:max-w-[20rem] font-montserrat bg-[#F3F3F3] text-[#273257] p-3 rounded-xl shadow-card text-lg xsm:text-xl">
+      <header className="text-lg xsm:text-xl font-azonix flex items-center gap-2">
+        {icon} {title}
+      </header>
+      <p>{text}</p>
     </div>
+
   );
 }
 
-export default Card;
+export { Card };
