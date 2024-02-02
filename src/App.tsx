a5e7cc177549e1bbae16ece721d0be87e3343bb6
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2024-02-02 13:29:12 -0300

    Add formulary placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { AboutUs } from "@components/AboutUs";
 import { ChooseOtimizza } from "@components/ChooseOtimizza";
 import { Footer } from "@components/Footer";
+import { Form } from "@components/Form";
 import { Header } from "@components/Header";
 import { Hero } from "@components/Hero";
 import { Services } from "@components/Services";
@@ -17,6 +18,7 @@ function App() {
         <AboutUs />
         <Services />
         <ChooseOtimizza/>
+        <Form />
         <Footer />
       </div>
     </SecaoProvider>
---
+++ src/components/Form/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Form(): JSX.Element {
+  return (
+    <div className="">
+        <div className="h-96"></div>
+            <h1 className="mx-auto  italic max-w-56 text-5xl">Formulário</h1>
+        <div className="h-96"></div>
+    </div>
+  );
+}
+
+export { Form };
