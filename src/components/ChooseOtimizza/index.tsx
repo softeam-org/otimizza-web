a653b19d94d1155c3450efc9b6b84175741c1b3b
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2024-05-21 18:22:26 -0300

    fix: replace assets images and change hover texts (#11)
    * chore: replace asset images
    
    * chore: Update text
    
    * fix: remade section styles
    
    * fix: fix text styles
    
    * fix: correct styles
    
    * fix: fix section alignment

--- src/components/ChooseOtimizza/index.tsx
+++ src/components/ChooseOtimizza/index.tsx
@@ -1,11 +1,12 @@
 import { ChooseCard } from "@components/ChooseCard"
 
-import Achievement from "../../assets/chooseIcons/achievement.svg"
-import Balance from "../../assets/chooseIcons/balance.svg"
-import Chart from "../../assets/chooseIcons/chart.svg"
-import Creative from "../../assets/chooseIcons/creative.svg"
-import Ecology from "../../assets/chooseIcons/ecology.svg"
-import NotePad from "../../assets/chooseIcons/notepad.svg"
+import ChartLineUp from "../../assets/chooseIcons/chartLineUp.svg"
+import GlobeHemisphereWest from "../../assets/chooseIcons/globeHemisphereWest.svg"
+import HandHeart from "../../assets/chooseIcons/handHeart.svg"
+import LightbulbFilament from "../../assets/chooseIcons/lightbulbFilament.svg"
+import Scales from "../../assets/chooseIcons/scales.svg"
+import Star from "../../assets/chooseIcons/star.svg"
+
 
 import './style.css'
 
@@ -18,51 +19,51 @@ interface CardProps {
 
 const cardsData: CardProps[] = [
   {
-    image: NotePad,
+    image: HandHeart,
     text: "RESPONSABILIDADE",
     alt: "imagem ilustrativa de um bloco de notas",
-    hover: "Uma vez fechado o acordo, nossa missão é com a entrega e satisfação."
+    hover: "Priorizamos a transparência, a qualidade e o comprometimento para garantir que cada projeto seja concluído com excelência."
   },
   {
-    image: Ecology,
+    image: GlobeHemisphereWest,
     text: "RESPEITO ÀS PESSOAS E MEIO AMBIENTE",
     alt: "Imagem ilustrativa do meio ambiente",
-    hover: "Afinal, buscamos melhorar a qualidade de vida dos nossos clientes."
+    hover: "Cultivamos uma cultura que prioriza a segurança, o bem-estar e a sustentabilidade em cada etapa de nossos projetos."
   },
   {
-    image: Balance,
-    text: "ÉTICA",
+    image: Scales,
+    text: "ETICA",
     alt: "Imagem ilustrativa de uma balança",
-    hover: "Honestidade e transparência guiam nossas ações."
+    hover: "Honestidade e transparência são o alicerce de nossas operações, orientando cada decisão que tomamos."
   },
   {
-    image: Chart,
+    image: ChartLineUp,
     text: "EFICIÊNCIA",
     alt: "Imagem ilustrativa de um gráfico",
     hover: "Trabalhamos com base em estratégias produtivas."
   },
   {
-    image: Creative,
+    image: LightbulbFilament,
     text: "INOVAÇÃO",
     alt: "Imagem ilustrativa de papel e lápis para representar o processo criativo",
     hover: "Resolvemos problemas complexos com soluções inovadoras."
   },
   {
-    image: Achievement,
+    image: Star,
     text: "ORGULHO DE SER OTIMIZZA",
     alt: "Imagem ilustrativa de uma medalha",
-    hover: "Acreditamos em uma Engenharia Acessível e Universal e na transformação que ela proporciona."
+    hover: "Acreditamos que a engenharia deve ser acessível e universal, inspirando soluções que transformam o mundo ao nosso redor."
   },
 ];
 
 function ChooseOtimizza(): JSX.Element {
   return (
-    <div className=" secao secao-servicos choose-otimizza flex w-full h-auto px-3 py-10 xlg:px-16 xlg:py-11 justify-center gap-2.5 font-azonix text-secondary">
-      <div className="flex flex-col p-4 items-center bg-[#DFEAEF] max-w-[1011px] gap-7">
+    <div className=" secao secao-servicos choose-otimizza w-full h-auto px-3 py-10 xlg:px-16 xlg:py-11 gap-2.5 font-azonix text-secondary">
+      <div className="flex flex-col mx-auto p-4 items-center bg-[#DFEAEF] max-w-[1011px] gap-7">
         <div className="text-xl md:text-2xl text-center lg:text-5xl">
           Por que escolher a Otimizza?
         </div>
-        <div className="flex flex-wrap gap-3 self-stretch	justify-center">
+        <div className="grid grid-cols-1 xsm:grid-cols-2 xl:grid-cols-3 gap-2.5">
           {cardsData.map(({ text, image, alt, hover }) => (
             <ChooseCard
               key={text}
