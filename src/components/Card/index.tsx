63121dc2853d8a0ac9c454aaa0317c048bd78556
Unaldo Santos Vasconcelos Neto <87950587+Uneto47@users.noreply.github.com>
2024-05-22 17:39:10 -0300

    fix: size cards and texts in abou us section (#13)
    * fix: icons, texts and positions
    
    * fix: card component adjustments
    
    * feat: add new responsive breakpoint
    
    * fix: font size and responsive breakpoint
    
    * fix: card and font size
    
    * fix: responsives breakpoints

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,17 +2,27 @@ interface CardProps {
   title: string;
   text: string;
   icon: React.ReactNode;
+  splitText?: boolean; // nova prop opcional
 }
 
-function Card({ title, text, icon }: CardProps): JSX.Element{
+function Card({ title, text, icon, splitText = false }: CardProps): JSX.Element {
+  const renderTextWithLineBreaks = (text: string) => {
+    return text.split(' ').map((word, index) => (
+      <span key={index} className="block">
+        {word}
+      </span>
+    ));
+  };
+
   return (
-    <div className="flex flex-col w-full md:max-w-[20rem] font-montserrat bg-snow-white text-primary p-3 rounded-xl shadow-card text-lg xsm:text-xl">
-      <header className="text-lg xsm:text-xl font-azonix flex items-center gap-2">
+    <div className="flex flex-col w-full min-w-60 md:max-w-[30rem] h-auto md:min-h-72 px-1 py-3 font-montserrat bg-snow-white text-primary rounded-xl shadow-card text-lg lg:text-xl text-center">
+      <header className="text-xl lg:text-2xl font-azonix flex items-center justify-center gap-2">
         {icon} {title}
       </header>
-      <p>{text}</p>
+      <div className="pt-3">
+      <p>{splitText ? renderTextWithLineBreaks(text) : text}</p>
+      </div>
     </div>
-
   );
 }
 
