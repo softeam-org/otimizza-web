d643811dc281ad48e6e402d2e585b2622ff6467c
Uneto47 <netovasconcelos02@gmail.com>
2024-05-21 22:58:08 -0300

    fix: card component adjustments

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,17 +2,27 @@ interface CardProps {
   title: string;
   text: string;
   icon: React.ReactNode;
+  splitText?: boolean; // nova prop opcional
 }
 
-function Card({ title, text, icon }: CardProps): JSX.Element{
+function Card({ title, text, icon, splitText = false }: CardProps): JSX.Element {
+  const renderTextWithLineBreaks = (text: string) => {
+    return text.split(' ').map((word, index) => (
+      <span key={index} className="block">
+        {word}
+      </span>
+    ));
+  };
+
   return (
-    <div className="flex flex-col w-full md:max-w-[20rem] font-montserrat bg-snow-white text-primary p-3 rounded-xl shadow-card text-lg xsm:text-xl">
-      <header className="text-lg xsm:text-xl font-azonix flex items-center gap-2">
+    <div className="flex flex-col w-full md:max-w-[28rem] min-h-72 px-3 pt-3 font-montserrat bg-snow-white text-primary rounded-xl shadow-card text-base xsm:text-lg text-center">
+      <header className="text-lg xsm:text-xl font-azonix flex items-center justify-center gap-2">
         {icon} {title}
       </header>
-      <p>{text}</p>
+      <div className="pt-3">
+      <p>{splitText ? renderTextWithLineBreaks(text) : text}</p>
+      </div>
     </div>
-
   );
 }
 
